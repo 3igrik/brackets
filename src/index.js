411a5fc93242ff514bdf84e0908bf5944ad18b3c
3igrik <3igrik@gmail.com>
2021-01-31 17:45:54 +0600

    fix: brackets task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  str = str.split('');
+
+  const openBrackets = [];
+  const closeBrackets = [];
+  const stack = [];
+
+  bracketsConfig.forEach(element => {
+    openBrackets.push(element[0]);
+    closeBrackets.push(element[1]);
+  });
+
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < openBrackets.length; j++){
+      if (openBrackets[j] == closeBrackets[j] && str[i] == closeBrackets[j]) {
+        if (str[i] == closeBrackets[stack[stack.length - 1]]) { 
+          stack.pop(j);
+        } else {
+          stack.push(j);
+        }
+      } else if (str[i] == openBrackets[j]) { 
+        stack.push(j);
+      } else if (str[i] == closeBrackets[j]) {
+        if (j == stack[stack.length - 1]) {
+          stack.pop();
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+  return stack.length == 0;
 }
